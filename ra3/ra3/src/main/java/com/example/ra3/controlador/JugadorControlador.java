f9c81cb7926be41abf949fd75a5c6b2956d1bed4
raulvt02 <vera.trashorras.raul@iescamas.es>
2026-01-19 12:07:06 +0100

    Actualizado JugadorServicio

--- ra3/ra3/src/main/java/com/example/ra3/controlador/JugadorControlador.java
+++ ra3/ra3/src/main/java/com/example/ra3/controlador/JugadorControlador.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.ra3.entidad.Jugador;
 import com.example.ra3.servicio.JugadorServicio;
 
 @Controller
--- ra3/ra3/src/main/java/com/example/ra3/servicio/JugadorServicio.java
+++ ra3/ra3/src/main/java/com/example/ra3/servicio/JugadorServicio.java
@@ -3,10 +3,12 @@ package com.example.ra3.servicio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.ra3.entidad.Jugador;
 import com.example.ra3.repositorio.JugadorRepositorio;
 
+@Service
 public class JugadorServicio {
 
 	@Autowired

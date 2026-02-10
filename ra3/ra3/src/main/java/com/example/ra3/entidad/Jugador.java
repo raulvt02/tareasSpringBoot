357aee2608d1b162f8ce05417826ff2987055467
raulvt02 <vera.trashorras.raul@iescamas.es>
2026-02-10 22:01:17 +0100

    Entidad Jugador actualizada

--- ra3/ra3/src/main/java/com/example/ra3/entidad/Jugador.java
+++ ra3/ra3/src/main/java/com/example/ra3/entidad/Jugador.java
@@ -5,15 +5,58 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
-@Table(name = "jugadores")
 public class Jugador {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(name = "nombre")
-	private String nombre;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotBlank(message = "El nickname no puede estar en blanco")
+    @Column(nullable = false)
+    private String nickname;
+
+    @Min(value = 13, message = "La edad mínima es 13 años")
+    private int edad;
+
+    @NotBlank(message = "El email no puede estar en blanco")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Email no válido")
+    @Column(nullable = false)
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

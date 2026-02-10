4ddf040d254b828c11d0b83c3235703d6c7da6dc
raulvt02 <vera.trashorras.raul@iescamas.es>
2026-02-10 22:07:48 +0100

    JugadorRespositorio actualizado

--- ra3/ra3/src/main/java/com/example/ra3/repositorio/JugadorRepositorio.java
+++ ra3/ra3/src/main/java/com/example/ra3/repositorio/JugadorRepositorio.java
@@ -1,9 +1,18 @@
 package com.example.ra3.repositorio;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.ra3.entidad.Jugador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface JugadorRepositorio extends JpaRepository<Jugador, Long>{
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface JugadorRepositorio extends JpaRepository<Jugador, Integer> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Jugador> findByEmail(String email);
+
+    List<Jugador> findByNicknameContainingIgnoreCase(String nickname);
 }
